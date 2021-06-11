9f0eb540ec2a653c8b3ac548b625940153614d55
Accro Crops <80210385+acccrop@users.noreply.github.com>
2021-06-11 13:21:20 +0530

    Update bigquery_access_policy.py

--- bigquery_access_policy.py
+++ bigquery_access_policy.py
@@ -8,8 +8,8 @@ import json
 class CKV_AWS_999(BaseResourceCheck):
     def __init__(self):
         self.logger = logging.getLogger("{}".format(self.__module__))
-        name = "Ensure That Ec2 has atleast one or more tags"
-        id = "CKV_AWS_999"
+        name = "Ensure That bigquery define the access parameter"
+        id = "CKV_GCP_999"
         supported_resources = '*'
         # CheckCategories are defined in models/enums.py
         categories = [CheckCategories.GENERAL_SECURITY]
@@ -17,10 +17,10 @@ class CKV_AWS_999(BaseResourceCheck):
 
     def scan_resource_conf(self, conf):
         self.logger.debug("ssss" + json.dumps(conf))
-        if 'tags' in conf.keys():
-            if len(conf['tags']) > 0:
+        if 'access' in conf.keys():
+            if len(conf['access']) > 0:
                 return CheckResult.PASSED     
         return CheckResult.FAILED
 
 
-scanner = CKV_AWS_999()
+scanner = CKV_GCP_999()
